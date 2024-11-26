2be277ac5c547186ecd93ba9b2d3af54d675728d
FerreiraRAf <rafaferreirarosa941@gmail.com>
2024-11-26 10:37:35 -0300

    Utilizando React fragments

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HelloWord from './components/HelloWord';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/Pessoa';
 import Frase from './components/Frase';
+import List from './components/List';
 
 function App() {
   const nome = "Maria";
@@ -17,6 +18,7 @@ function App() {
       <SayMyName nome="João" />
       <SayMyName nome={nome} />
       <Pessoa nome="Rafael" idade="20" profissao="Programador" foto="https://via.placeholder.com/150" />
+      <List />
 
     </div>
   );
